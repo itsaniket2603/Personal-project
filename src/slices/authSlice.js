dd24ff1c62de726396b4c689993b74006b7328ce
Aniket <its.aniket26@gmail.com>
2025-06-06 14:45:21 +0530

    Clean first commit

---
+++ src/slices/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  token: localStorage.getItem("token") || null, // ✅ fixed here
+  loading: false,
+  signupData: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    setToken(state, action) {
+      state.token = action.payload;
+    },
+    setLoading(state, action) {
+      state.loading = action.payload;
+    },
+    setSignupData(state, action) {
+      state.signupData = action.payload;
+    },
+  },
+});
+
+export const { setToken, setLoading, setSignupData } = authSlice.actions;
+export default authSlice.reducer;
